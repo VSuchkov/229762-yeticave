84722a020f80ce89c29c79c55bc8bfc85ea91dde
Vladimir Suchkov <ssvovs@mail.ru>
2018-04-28 21:25:03 +0300

    правки

--- index.php
+++ index.php
@@ -70,25 +70,25 @@ function fsr($num) {
         </form>
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
         <nav class="user-menu">
-        <?php
-            if ($is_auth == true) {
-                      echo '<div class="user-menu__image">
-                        <img src="" width="40" height="40" alt="Пользователь">
-                      </div>
-                      <div class="user-menu__logged">
-                        <p></p>
-                      </div>';
-            } else {
-                    echo '<ul class="user-menu__list">
-                        <li class="user-menu__item">
-                          <a href="#">Регистрация</a>
-                        </li>
-                        <li class="user-menu__item">
-                          <a href="#">Вход</a>
-                        </li>
-                      </ul>';
-            };
-        ?>
+            
+        <?php if ($is_auth == true) { ?>
+            <div class="user-menu__image">
+                <img src="" width="40" height="40" alt="Пользователь">
+            </div>
+            <div class="user-menu__logged">
+                <p></p>
+            </div>
+        <?php } else { ?>
+          <ul class="user-menu__list">
+             <li class="user-menu__item">
+               <a href="#">Регистрация</a>
+             </li>
+             <li class="user-menu__item">
+               <a href="#">Вход</a>
+             </li>
+           </ul>
+        <?php } ?>
+        
         </nav>
     </div>
 </header>
